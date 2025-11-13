9b6c9cf72d73eecdad221a2ff1266e87ce0689dd
magnusgudmundsrud <mgudmundsrud@gmail.com>
2025-11-13 16:36:57 +0100

    La til LazyColumn i AnimeScreen

--- app/src/main/java/com/example/androideksamen/screens/anime/AnimeScreen.kt
+++ app/src/main/java/com/example/androideksamen/screens/anime/AnimeScreen.kt
@@ -1,17 +1,35 @@
 package com.example.androideksamen.screens.anime
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.example.androideksamen.navigation.NavRoutes
 
 @Composable
 fun AnimeScreen(
     animeViewModel: AnimeViewModel,
     navController: NavController
 ) {
+    val animes = animeViewModel.animes.collectAsState()
 
     Column() {
         Text("AnimeScreen")
+
+        LazyColumn() {
+            items(animes.value) { anime ->
+                AnimeList(
+                    anime,
+                    seeDetails = {
+                        navController.navigate(
+                            NavRoutes.AnimeDetailsRoute(
+                                anime.id
+                            )
+                        )
+                    }
+                )
+            }
+        }
     }
 }

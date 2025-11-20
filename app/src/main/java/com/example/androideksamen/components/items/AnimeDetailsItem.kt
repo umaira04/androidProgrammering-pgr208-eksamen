896e6dd2bc4ba3f0115cb29ec2d3696704cc7e6f
Jeanette Källström <jeanettekallstrom@Jeanette-sin-MacBook-Pro.local>
2025-11-20 12:24:34 +0100

    Startet med box og column på AnimeDetailsItem

--- app/src/main/java/com/example/androideksamen/components/items/AnimeDetailsItem.kt
+++ app/src/main/java/com/example/androideksamen/components/items/AnimeDetailsItem.kt
@@ -1,1 +1,37 @@
 package com.example.androideksamen.components.items
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.androideksamen.data.api.Anime
+
+@Composable
+fun AnimeDetailsItem(anime : Anime) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(
+                color = Color(0xFFD0D0D0),
+                shape = RoundedCornerShape(8.dp)
+            )
+    ){
+        Column(
+
+        ) {
+
+        } // End Column
+
+    } // End Box
+} // End AnimeDetailsItem

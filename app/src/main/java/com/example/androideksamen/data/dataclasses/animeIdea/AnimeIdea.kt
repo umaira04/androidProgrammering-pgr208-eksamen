282c1c156049a475acc02abcfcbb3028f6219b51
magnusgudmundsrud <mgudmundsrud@gmail.com>
2025-11-22 11:04:46 +0100

    test

--- app/src/main/java/com/example/androideksamen/data/dataclasses/animeIdea/AnimeIdea.kt
+++ app/src/main/java/com/example/androideksamen/data/dataclasses/animeIdea/AnimeIdea.kt
@@ -1,8 +1,12 @@
 package com.example.androideksamen.data.dataclasses.animeIdea
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.androideksamen.data.database.Genre
 
+@Entity
 data class AnimeIdea(
+    @PrimaryKey
     val id: Int,
     val title: String,
     val genre: Genre,

8d0d4d4bc2f774089c7bb6095749c69d589f66a3
magnusgudmundsrud <mgudmundsrud@gmail.com>
2025-11-22 13:43:45 +0100

    får ikke kjørt

--- app/src/main/java/com/example/androideksamen/components/items/AnimeItem.kt
+++ app/src/main/java/com/example/androideksamen/components/items/AnimeItem.kt
@@ -7,14 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,7 +23,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import com.example.androideksamen.data.dataclasses.anime.Anime
 import com.example.androideksamen.data.dataclasses.anime.Genre
@@ -37,8 +34,6 @@ fun AnimeItem(
 ) {
     Box(
         modifier = Modifier
-            .fillMaxWidth()
-            // .height(576.dp)
             .padding(8.dp)
             .clickable{showDetails()}
             .background(
@@ -58,7 +53,11 @@ fun AnimeItem(
 
                 Spacer(modifier = Modifier.height(4.dp))
 
-                Row() {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(402.dp)
+                ) {
                     Box(
                         modifier = Modifier
                             .width(270.dp)
@@ -79,7 +78,6 @@ fun AnimeItem(
                             maxLines = 1,
                             modifier = Modifier
                                 .rotate(90f)
-                                .width(402.dp)
                         )
                     }
                 }

5e9573d2aaeaa520eb93b7448c151c1146df8899
magnusgudmundsrud <mgudmundsrud@gmail.com>
2025-11-23 21:55:10 +0100

    Styling for CharacterItem

--- app/src/main/java/com/example/androideksamen/components/items/CharacterItem.kt
+++ app/src/main/java/com/example/androideksamen/components/items/CharacterItem.kt
@@ -1,76 +1,110 @@
 package com.example.androideksamen.components.items
 
+import android.R.attr.fontWeight
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.androideksamen.data.dataclasses.character.Character
 
+
+//TODO: legge til fav
+//TODO: filter etter fav
+//TODO: søk
+//TODO: endre fontSize i animeitem så det er likt som characteritem
 @Composable
 fun CharacterItem(
-    character: Character
+    character: Character,
+    isFavorite: Boolean = false
 ) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
             .background(
-                color = Color(0xFFD0D0D0),
+                color = Color(0xFFFDF1B2),
                 shape = RoundedCornerShape(8.dp)
             )
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(96.dp)
-                .padding(8.dp)
+                .height(104.dp)
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Column(
-                modifier = Modifier.padding(start = 8.dp)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 8.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+
             ) {
                 Text(
                     text = character.name,
-                    fontSize = 20.sp,
+                    fontSize = if (character.name.toString().length > 16) 16.sp else 24.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
                     fontWeight = FontWeight.Bold
                 )
 
-                Spacer(modifier = Modifier.height(4.dp))
-
                 Text(
                     text = character.nameJapanese ?: "No japanese name",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+
+                Text(
+                    text = "Noe info her også",
                     fontSize = 16.sp
                 )
             }
 
             // TODO : legge inn link til myanimelist.com wiki til karakteren?
+            
+            IconButton(
+                onClick = {},
+                modifier = Modifier.size(40.dp)
+            ) {
+                Icon(
+                    imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
+                    contentDescription = "Favorite",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
 
-            Spacer(modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.width(8.dp))
 
             AsyncImage(
                 model = character.characterImage?.jpg?.imageUrl,
-                contentDescription = "bilde av ${character.name}",
+                contentDescription = "Bilde av ${character.name}",
                 modifier = Modifier
-                    .padding(end = 8.dp)
-                    .size(120.dp, 80.dp)
-                    .background(
-                        color = Color.Black,
-                        shape = RoundedCornerShape(8.dp)
-                    )
+                    .size(80.dp)
             )
         }
     }
@@ -85,9 +119,10 @@ fun CharacterItemPreview() {
             name = "Pikachu",
             characterImage = null,
             nameJapanese = "Japansk Pikachu",
-
-
-            )
+            ),
+        isFavorite = false
     )
+
+   
 }
 

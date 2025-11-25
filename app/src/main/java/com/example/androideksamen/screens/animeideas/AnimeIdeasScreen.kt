d8633841f05916d82575b458eef41f0ea8e5771b
jeanettebk <jeanette.bach.kallstrom@gmail.com>
2025-11-25 20:47:23 +0100

    ideaScreen

--- app/src/main/java/com/example/androideksamen/screens/animeideas/AnimeIdeasScreen.kt
+++ app/src/main/java/com/example/androideksamen/screens/animeideas/AnimeIdeasScreen.kt
@@ -1,6 +1,11 @@
 package com.example.androideksamen.screens.animeideas
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -12,6 +17,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androideksamen.components.items.AnimeIdeaItem
 import com.example.androideksamen.data.database.AnimeDB
 
@@ -39,8 +50,32 @@ fun AnimeIdeasScreen(
         animeIdeasViewModel.deleteAnimeIdea(animeIdea)
     }
 
-    Column {//MAIN COLUMN START
-        Text("AnimeIdeasScreen")
+    Column ( //MAIN COLUMN START
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFFDF1B2))
+                .padding(8.dp, 8.dp, 8.dp, 0.dp)
+
+    ){
+        Text(
+            "Anime ideas",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF324663),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .fillMaxWidth()
+        )
+
+        Text(
+            "Make your personal anime ideas!",
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
 
         TextField(
             value = title,

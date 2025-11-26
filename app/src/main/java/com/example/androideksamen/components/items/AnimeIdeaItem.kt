4fc6425c4a7ff78539c7c732b165508162c88d61
jeanettebk <jeanette.bach.kallstrom@gmail.com>
2025-11-26 14:44:11 +0100

    Jobbet videre med animeIdeaScreen. Styling + funksjonalitet for knapper som varning ved sletting

--- app/src/main/java/com/example/androideksamen/components/items/AnimeIdeaItem.kt
+++ app/src/main/java/com/example/androideksamen/components/items/AnimeIdeaItem.kt
@@ -5,18 +5,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androideksamen.data.database.AnimeDB
 
 //TODO: LAGE ET ITEM SOM KAN VISE IDEER SOM BRUKEREN HAR LAGRET
@@ -29,8 +38,12 @@ fun AnimeIdeaItem(
 ) {
     Box(
         modifier = Modifier
-            .background(Color(0xFFa4facb))
             .fillMaxWidth()
+            .padding(8.dp)
+            .background(
+                color = Color(0xFF90FDE6),
+                shape = RoundedCornerShape(8.dp)
+            )
     ) {
         Column {
             Row(
@@ -39,25 +52,47 @@ fun AnimeIdeaItem(
                     .fillMaxWidth()
                     .padding(8.dp)
             ) {
-                Column {
-                    Text(animeIdea.title)
-                    Text(animeIdea.synopsis)
-                }
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                            modifier = Modifier
+                                .padding(8.dp)
                 ) {
-                    Button(
-                        onClick = { handleEditBtnClick(animeIdea) }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.End),
+                        modifier = Modifier.fillMaxWidth()
                     ) {
-                        Icon(Icons.Outlined.Edit, contentDescription = "none")
-                    }
-                    Button(
-                        onClick = { handleDeleteBtnClick(animeIdea) }
-                    ) {
-                        Icon(Icons.Outlined.Delete, contentDescription = "none")
-                    }
+                        IconButton(
+                            onClick = { handleDeleteBtnClick(animeIdea) },
+                            modifier = Modifier
+                                .size(48.dp)
+                                .clip(RoundedCornerShape(25.dp))
+                        ) {
+                            Icon(Icons.Outlined.Delete, contentDescription = "none")
+                        }
+                        IconButton(
+                            onClick = { handleEditBtnClick(animeIdea) },
+                            modifier = Modifier
+                                .size(48.dp)
+                                .clip(RoundedCornerShape(25.dp))
+                        ) {
+                            Icon(Icons.Outlined.Edit, contentDescription = "none")
+                        }
 
+                    }
+                    Text(
+                        text = "Title",
+                    color = Color(0xFF656391),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                    )
+                    Text(animeIdea.title)
+                    Text(
+                        text = "Synopsis",
+                        color = Color(0xFF656391),
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(animeIdea.synopsis)
                 }
             }
         }
--- app/src/main/java/com/example/androideksamen/screens/animeideas/AnimeIdeasScreen.kt
+++ app/src/main/java/com/example/androideksamen/screens/animeideas/AnimeIdeasScreen.kt
@@ -1,23 +1,35 @@
 package com.example.androideksamen.screens.animeideas
 
+import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -37,7 +49,8 @@ fun AnimeIdeasScreen(
     var id: Int by remember { mutableStateOf(value = 0) }
 
     var isEditing by remember { mutableStateOf(false) }
-    //var editedAnimeIdea: AnimeDB? by remember { mutableStateOf(null) }
+    var isDeleting by remember { mutableStateOf(false) }
+    var animeIdeaToDelete: AnimeDB? by remember { mutableStateOf(null) }
 
     fun handleEditBtnClick(animeIdea: AnimeDB) {
         title = animeIdea.title
@@ -47,17 +60,18 @@ fun AnimeIdeasScreen(
     }
 
     fun handleDeleteBtnClick(animeIdea: AnimeDB) {
-        animeIdeasViewModel.deleteAnimeIdea(animeIdea)
+        isDeleting = true
+        animeIdeaToDelete = animeIdea
     }
 
-    Column ( //MAIN COLUMN START
+    Column( //MAIN COLUMN START
         verticalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFFFDF1B2))
-                .padding(8.dp, 8.dp, 8.dp, 0.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFDF1B2))
+            .padding(8.dp, 8.dp, 8.dp, 0.dp)
 
-    ){
+    ) {
         Text(
             "Anime ideas",
             fontSize = 28.sp,
@@ -80,40 +94,74 @@ fun AnimeIdeasScreen(
         TextField(
             value = title,
             onValueChange = { title = it },
-            label = { Text("Tittel") }
+            label = { Text("Tittel") },
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(32.dp))
+                .fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.LightGray,
+                focusedIndicatorColor = Color.Gray,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedPlaceholderColor = Color(0xFFF7EAF9),
+                unfocusedPlaceholderColor = Color(0xFFF7EAF9),
+                focusedTextColor = Color.Black,
+                unfocusedTextColor = Color.Gray
+            ),
         )
         TextField(
             value = synopsis,
             onValueChange = { synopsis = it },
-            label = { Text("Synopsis") }
+            label = { Text("Synopsis") },
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(32.dp))
+                .fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.LightGray,
+                focusedIndicatorColor = Color.Gray,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedPlaceholderColor = Color(0xFFF7EAF9),
+                unfocusedPlaceholderColor = Color(0xFFF7EAF9),
+                focusedTextColor = Color.Black,
+                unfocusedTextColor = Color.Gray
+            ),
         )
 
 
         if (isEditing) {
-            Button(
-                onClick = {
-                    if (title.isNotEmpty() && synopsis.isNotEmpty()) {
-                        animeIdeasViewModel.updateAnimeIdea(
-                            AnimeDB(id = id, title = title, synopsis = synopsis)
-                        )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier
 
+            ) {
+                Button(
+                    onClick = {
+                        if (title.isNotEmpty() && synopsis.isNotEmpty()) {
+                            animeIdeasViewModel.updateAnimeIdea(
+                                AnimeDB(id = id, title = title, synopsis = synopsis)
+                            )
+
+                            isEditing = false
+                            title = ""
+                            synopsis = ""
+                        }
+                    }
+                ) {
+                    Text("Save changes")
+                }
+
+                Button(
+                    onClick = {
                         isEditing = false
                         title = ""
                         synopsis = ""
                     }
+                ) {
+                    Text("Cancel")
                 }
-            ) {
-                Text("Save changes")
-            }
-
-            Button(
-                onClick = {
-                    isEditing = false
-                    title = ""
-                    synopsis = ""
-                }
-            ) {
-                Text("Cancel")
             }
         } else {
 
@@ -123,6 +171,8 @@ fun AnimeIdeasScreen(
                         animeIdeasViewModel.insertAnimeIdea(
                             AnimeDB(title = title, synopsis = synopsis)
                         )
+                        title = ""
+                        synopsis = ""
                     }
                 }
             ) {
@@ -130,19 +180,77 @@ fun AnimeIdeasScreen(
             }
         }
 
-        if (animeIdeas.value.count() > 0) {
-            LazyColumn {
-                items(animeIdeas.value) { animeIdea ->
-                    AnimeIdeaItem(
-                        animeIdea,
-                        handleEditBtnClick = { handleEditBtnClick(animeIdea = it) },
-                        handleDeleteBtnClick = { handleDeleteBtnClick(animeIdea = it) }
+        if (isDeleting) {
+
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.White)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Warning,
+                        contentDescription = "none",
+                        tint = Color.Red,
+                        modifier = Modifier.size(64.dp)
                     )
+                    Text("Are you sure you want to delete: ")
+                    Text(
+                        text = animeIdeaToDelete?.title ?: "unknown",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier.padding(top=16.dp)
+
+                    ) {
+                        Button(
+                            onClick = {
+                                animeIdeaToDelete?.let { animeIdea ->
+                                    animeIdeasViewModel.deleteAnimeIdea(animeIdea)
+                                }
+                                isDeleting = false
+                                animeIdeaToDelete = null
+                            }
+                        ) {
+                            Text("Delete")
+                        }
+
+                        Button(
+                            onClick = {
+                                isDeleting = false
+                                animeIdeaToDelete = null
+                            }
+                        ) {
+                            Text("Cancel")
+                        }
+                    }
                 }
             }
         } else {
-            Text("No anime ideas found")
+            if (animeIdeas.value.count() > 0 && !isDeleting) {
+                LazyColumn {
+                    items(animeIdeas.value) { animeIdea ->
+                        AnimeIdeaItem(
+                            animeIdea,
+                            handleEditBtnClick = { handleEditBtnClick(animeIdea = it) },
+                            handleDeleteBtnClick = { handleDeleteBtnClick(animeIdea = it) }
+                        )
+                    }
+                }
+            } else {
+                Text("No anime ideas found")
+            }
         }
-
     } //MAIN COLUMN END
 }//AnimeIdeasScreen END

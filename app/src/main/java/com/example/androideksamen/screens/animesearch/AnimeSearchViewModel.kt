26661ab61a0dcbab9d536991edac020721cb64fb
magnusgudmundsrud <mgudmundsrud@gmail.com>
2025-11-28 20:06:37 +0100

    flyttet logikk fra searchscreen til searchviewmodel

--- app/src/main/java/com/example/androideksamen/screens/animesearch/AnimeSearchViewModel.kt
+++ app/src/main/java/com/example/androideksamen/screens/animesearch/AnimeSearchViewModel.kt
@@ -13,10 +13,27 @@ class AnimeSearchViewModel : ViewModel() {
 
     //FORELESNING 9 FOR Å SE AKKURAT DENNE OPPGAVEN
     private val _anime = MutableStateFlow<Anime?>(null)
-    private val _mainCharacters = MutableStateFlow<List<Character>>(emptyList())
     val anime = _anime.asStateFlow()
+
+    private val _mainCharacters = MutableStateFlow<List<Character>>(emptyList())
     val mainCharacters = _mainCharacters.asStateFlow()
 
+    private val _id = MutableStateFlow("")
+    val id = _id.asStateFlow()
+
+    private val _isSearched = MutableStateFlow(false)
+    val isSearched = _isSearched.asStateFlow()
+
+    fun setId(value: String) { _id.value = value }
+
+    fun searchAnime() {
+        val idParsed = _id.value.toIntOrNull()
+        if (idParsed != null) {
+            setAnimeById(idParsed)
+            setAnimeMainCharactersByAnimeId(idParsed)
+            _isSearched.value = true
+        }
+    }
 
     fun setAnimeById(id: Int) {
         viewModelScope.launch {

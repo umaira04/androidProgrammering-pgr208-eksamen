7d9322938d34dc95f640a0eda92deb06abf0d54b
umaira04 <umah2706@gmail.com>
2025-12-01 17:48:46 +0100

    kjørt reformat code

--- app/src/main/java/com/example/androideksamen/MainActivity.kt
+++ app/src/main/java/com/example/androideksamen/MainActivity.kt
@@ -9,10 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.androideksamen.data.database.AnimeDbRepository
 import com.example.androideksamen.navigation.AppNavigation
 import com.example.androideksamen.screens.anime.AnimeViewModel
